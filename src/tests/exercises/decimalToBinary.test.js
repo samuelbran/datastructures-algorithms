b75525895c1709e9e39b3419b56fb833541459e6
Samuel Bran <hello@samuelbran.com>
2022-08-04 21:05:31 -0600

    reset everything

--- src/tests/exercises/decimalToBinary.test.js
+++
@@ -1,7 +0,0 @@
-const decimalToBinary = require("../../exercises/decimalToBinary");
-
-describe("Decimal to Binary function", () => {
-  test("It should return '100101110' when 302 is given", () => {
-    expect(decimalToBinary(302)).toBe("100101110");
-  });
-});
