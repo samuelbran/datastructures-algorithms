5bced231806394eee8e56942c2246c46095130d2
Samuel Bran <hello@samuelbran.com>
2022-08-20 22:20:55 -0600

    Updated Stack and Queue

--- src/Queue.js
+++ src/Queue.js
@@ -15,7 +15,6 @@ class Queue {
 
   enqueue(value) {
     const newNode = new Node(value);
-
     if (this.length === 0) {
       this.first = newNode;
       this.last = newNode;
@@ -23,15 +22,13 @@ class Queue {
       this.last.next = newNode;
       this.last = newNode;
     }
-
     this.length++;
     return this;
   }
 
   dequeue() {
     if (this.length === 0) return undefined;
-    let temp = this.first;
-
+    const temp = this.first;
     if (this.length === 1) {
       this.first = null;
       this.last = null;
@@ -39,9 +36,7 @@ class Queue {
       this.first = this.first.next;
       temp.next = null;
     }
-
     this.length--;
-
     return temp;
   }
 }
