830302f0fb715274ddd1a11203e6afb4791b48c6
Samuel Bran <hello@samuelbran.com>
2022-08-31 21:09:42 -0600

    insertionSort

---
+++ src/algorithms/insertionSort.js
@@ -0,0 +1,13 @@
+function insertionSort(array) {
+  let temp;
+  for (let i = 1; i < array.length; i++) {
+    temp = array[i];
+    for (var j = i - 1; array[j] > temp && j > -1; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+  return array;
+}
+console.log(insertionSort([5, 6, 3, 2, 1]));
+module.exports = insertionSort;
